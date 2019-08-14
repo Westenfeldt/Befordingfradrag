b23df3335a9fabd26590472c047024e5d6d278a4
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-14 13:51:27 +0200

    Add project files.

---
+++ Befordingfradrag/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Befordingfradrag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hvor langt er der til dit arbejdet?");
+            string afstand;
+            double distance;
+            afstand = Console.ReadLine();
+            distance = float.Parse(afstand);
+            Console.WriteLine("Du har angivet at du har {0} antak km til arbejde", afstand);
+            
+            //Beregning
+
+            if (distance < 24)
+            {
+                Console.WriteLine("Du kan ikke modtage fradraget");
+            }
+            else if (distance >= 24 && distance <= 100)
+            {
+                Console.WriteLine("Du kan modtage 154 øre pr km");
+            }
+            else if (distance > 100)
+            {
+                Console.WriteLine("Du kan modtage 77 øre pr km");
+            }
+
+
+        }
+    }
+}
