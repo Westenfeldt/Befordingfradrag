2e5a4ce5d6d07979145dfb4e0e4f194d3b0b62ea
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-14 14:03:40 +0200

    Tilføjet udregning af fradrag

--- Befordingfradrag/Program.cs
+++ Befordingfradrag/Program.cs
@@ -9,6 +9,7 @@ namespace Befordingfradrag
             Console.WriteLine("Hvor langt er der til dit arbejdet?");
             string afstand;
             double distance;
+            double total;
             afstand = Console.ReadLine();
             distance = float.Parse(afstand);
             Console.WriteLine("Du har angivet at du har {0} antak km til arbejde", afstand);
@@ -22,10 +23,19 @@ namespace Befordingfradrag
             else if (distance >= 24 && distance <= 100)
             {
                 Console.WriteLine("Du kan modtage 154 øre pr km");
+
+                total = (distance * 154) / 100;
+
+                Console.WriteLine("Du kan modtage {0} kr i fradrag", total);
             }
             else if (distance > 100)
             {
                 Console.WriteLine("Du kan modtage 77 øre pr km");
+
+                total = (distance * 77) / 100;
+
+                Console.WriteLine("Du kan modtage {0} kr i fradrag", total);
+
             }
 
 
